b99c52a0fe485723f58e5f4c3dd53b39bf8b726f
Svb home <sergio.valverde.b@gmail.com>
2017-10-15 19:55:28 -0600

    Implementing Google 0auth login (partial)

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const express = require("express");
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const keys = require("./config/keys");
+
 const app = express();
 
 //Route handler
-app.get("*", (req, res) => {
-  res.send({ hi: "there" });
-});
+// app.get("/", (req, res) => {
+//   res.send({ hi: "there" });
+// });
+
+passport.use(
+  new GoogleStrategy({
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret
+  })
+);
 
 /*
 Environment variables injected by Heroku
