dc6b3b3b3e06311e829a0c8f4378c569578fc3c0
Sergio Valverde <sergio.valverde.b@gmail.com>
2017-10-16 10:12:58 -0600

    Setting up calls to Google 0auth and appropriate callback to receive identifying information for a user

--- index.js
+++ index.js
@@ -1,26 +1,44 @@
-const express = require("express");
-const passport = require("passport");
-const GoogleStrategy = require("passport-google-oauth20").Strategy;
-const keys = require("./config/keys");
+const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
 
 const app = express();
 
 //Route handler
-// app.get("/", (req, res) => {
-//   res.send({ hi: "there" });
+// app.get('/', (req, res) => {
+//   res.send({ hi: 'there' });
 // });
 
 passport.use(
-  new GoogleStrategy({
-    clientID: keys.googleClientID,
-    clientSecret: keys.googleClientSecret
-  })
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: '/auth/google/callback'
+    },
+    (accessToken, refreshToken, profile, done) => {
+      // console.log('\naccessToken:', accessToken);
+      // console.log('\nrefreshToken:', refreshToken);
+      // console.log('\nprofile:', profile);
+      // console.log('\ndone:', done);
+    }
+  )
 );
 
+app.get(
+  '/auth/google',
+  passport.authenticate('google', {
+    //Authenticate using google strategy
+    scope: ['profile', 'email'] //Permissions that we are asking for, from a user's information - Not necessary for this app.
+  })
+);
+app.get('/auth/google/callback', passport.authenticate('google'));
+
 /*
-Environment variables injected by Heroku
-Environment variables are variables set by the underlying runtime that node
-is running on top of
+Environment variables injected by Heroku.
+Environment variables are variables set by 
+the underlying runtime that node is running on top of
 */
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
