076b15ec71c0b5c6ba38568e30d3d5760e517dd2
Sergio Valverde <sergio.valverde.b@gmail.com>
2017-10-23 15:29:25 -0600

    Defining very basic initial versions of client routes

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 
-export default class App extends React.Component {
-  render() {
-    return <h1>Hello!</h1>;
-  }
-}
+import Header from './Header';
+const Dashboard = () => <h2>Dashboard</h2>;
+const SurveyNew = () => <h2>SurveyNew</h2>;
+const Landing = () => <h2>Landing</h2>;
+
+const App = () => (
+  <div>
+    <BrowserRouter>
+      <div>
+        <Header />
+        <Route exact path="/" component={Landing} />
+        <Route exact path="/survey" component={Dashboard} />
+        <Route exact path="/survey/new" component={SurveyNew} />
+      </div>
+    </BrowserRouter>
+  </div>
+);
+export default App;
