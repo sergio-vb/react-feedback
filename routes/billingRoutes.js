858f76552406be12aad01dfe984dc313381163c9
Sergio Valverde <sergio.valverde.b@gmail.com>
2017-10-30 15:54:24 -0600

    Creating a Stripe charge on the back end

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -2,7 +2,14 @@ const keys = require('../config/keys');
 const stripe = require('stripe')(keys.stripeSecretKey);
 
 module.exports = app => {
-  app.post('/api/stripe', (req, res) => {
-
+  //Creates a Stripe charge after, receives the appropriate Stripe token
+  app.post('/api/stripe', async (req, res) => {
+    const charge = await stripe.charges.create({
+      amount: 500,
+      currency: 'usd',
+      description: '5 survey credits for $5',
+      source: req.body.id
+    });
+    console.log(charge);
   });
 };
