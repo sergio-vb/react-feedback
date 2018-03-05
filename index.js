2a1f69d103163d09b2feb6136bbb0b10b0362a43
sergio-vb <sergio.valverde.b@gmail.com>
2018-03-05 14:07:19 -0600

    Fixing typo in index.js

--- index.js
+++ index.js
@@ -41,9 +41,9 @@ if (process.env.NODE_ENV === 'production'){
 
   //Serve index.html for unrecognized routes
   const path = require('path');
-  app.get('*', (req, res => {
+  app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  }));
+  });
 }
 
 /*
